a4ee1ba2624bf1236f3cbcdbf687876f478abcb2
softwaretestingcentre <nicktulett@gmail.com>
2025-05-21 22:37:12 +0100

    Glue config param

--- src/test/java/com/softwaretestingcentre/testjobportal/CucumberTestSuite.java
+++ src/test/java/com/softwaretestingcentre/testjobportal/CucumberTestSuite.java
@@ -6,14 +6,12 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
-@CucumberOptions(
-  features = "src/test/resources/features",
-  glue = {"src/test/java/com/softwaretestingcentre/testjobportal/stepdefinitions"}
-)
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com/softwaretestingcentre/testjobportal/stepdefinitions")
 public class CucumberTestSuite {
 }
